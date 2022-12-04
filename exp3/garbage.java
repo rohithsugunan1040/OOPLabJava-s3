46f0dd165e6fe6c18d88ccc1b697a01adf3ea7f1
Rohith P S <96838745+rohithsugunan1040@users.noreply.github.com>
2022-12-04 12:27:29 +0530

    Added garbage collector and abstract class

---
+++ exp3/garbage.java
@@ -0,0 +1,27 @@
+package cycle3;
+
+public class garbage {
+
+    public String temp = "dsfadfgdfadsgsdfgasg";
+    int[] i = new int[1000];
+
+    public static void main(String[] args) {
+
+        Runtime r = Runtime.getRuntime();
+
+        long memoryBeforeObjectCreation= r.freeMemory();
+
+        long memoryAfterObjectCreation = r.freeMemory();
+
+        long memoryAfterObjectSetToNull = r.freeMemory();
+
+        r.gc();
+
+        long memoryAfterGarbageCollection = r.freeMemory();
+
+        System.out.println("Memory before object creation: " + memoryBeforeObjectCreation);
+        System.out.println("Memory after object creation: " + memoryAfterObjectCreation);
+        System.out.println("Memory after object set to null: " + memoryAfterObjectSetToNull);
+        System.out.println("Memory after garbage collection: " + memoryAfterGarbageCollection);
+    }
+}
